d41e6a6ad020dd3277bd9f994a04674bd0d2a27c
Vero <107260748+sixteenangel@users.noreply.github.com>
2022-09-14 23:19:12 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import BasicExample from './components/Cards';
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+<BasicExample/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,24 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CartWidget from "./CartWidget";
+
+
+function BasicExample() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="secondary">  Añadir   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+</svg>   </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default BasicExample;
