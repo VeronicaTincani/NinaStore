f4871e76fa77b524c50c72d91261e0b950402784
Vero <107260748+sixteenangel@users.noreply.github.com>
2022-09-20 13:55:56 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Productos from './conteiners/ItemListContainer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Productos/> 
+    </>
+  );
+}
+
+export default App;
